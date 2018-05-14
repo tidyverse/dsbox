ec3dc16e91067a532df8b60e9a999a3682de89b2
Mine Cetinkaya-Rundel <mine@stat.duke.edu>
2018-05-14 10:13:15 -0400

    Fisheries and instructor trends datasets

---
+++ R/fisheries.R
@@ -0,0 +1,16 @@
+#' Fishing industry by country
+#'
+#' World fisheries harvest for 2005. The tonnage from capture and aquaculture
+#' is listed by country. Countries which harvested less than 100,000 tons are
+#' not included.
+#'
+#' @source https://en.wikipedia.org/wiki/Fishing_industry_by_country
+#' @format A tibble with 75 rows and 3 variables:
+#' \describe{
+#' \item{country}{Name of country}
+#' \item{capture}{Tonnage from capture, including fish, crustaceans, molluscs, etc.}
+#' \item{aquaculture}{Tonnage from aquaculture}
+#' }
+#' @examples
+#' fisheries
+"fisheries"
---
+++ R/instructors.R
@@ -0,0 +1,23 @@
+#' Instructional staff employee trends
+#'
+#' The American Association of University Professors (AAUP) is a nonprofit
+#' membership association of faculty and other academic professionals. This
+#' dataset released by the AAUP shows trends in instructional staff employees
+#' between 1975 and 2011. Values given are percentages of full-time tenured,
+#' full-time tenure-track, full-time non-tenure-track, and part-time faculty
+#' as well as graduate student employees for each year in the dataset. Values
+#' may not add up to 100\% for a given year due to rounding errors.
+#'
+#' @source https://www.aaup.org/sites/default/files/files/AAUP_Report_InstrStaff-75-11_apr2013.pdf
+#' @format A tibble with 11 rows and 6 variables:
+#' \describe{
+#' \item{year}{Year}
+#' \item{full_time_tenured}{Percentage of Full-Time Tenured Faculty}
+#' \item{full_time_tenure_track}{Percentage of Full-Time Tenure-Track Faculty}
+#' \item{full_time_non_tenure_track}{Percentage of Full-Time Non-Tenure-Track Faculty}
+#' \item{part_time}{Percentage of Part-Time Faculty}
+#' \item{grad_student}{Percentage of Graduate Student Employees}
+#' }
+#' @examples
+#' instructors
+"instructors"
