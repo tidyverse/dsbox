e27381d91210f94af1c782716ab8a29a0a95f1c6
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-08-07 12:57:09 +0100

    Make a clickable URL

--- R/fisheries.R
+++ R/fisheries.R
@@ -4,7 +4,7 @@
 #' is listed by country. Countries which harvested less than 100,000 tons are
 #' not included.
 #'
-#' @source https://en.wikipedia.org/wiki/Fishing_industry_by_country
+#' @source \url{https://en.wikipedia.org/wiki/Fishing_industry_by_country}
 #' @format A tibble with 75 rows and 3 variables:
 #' \describe{
 #'   \item{country}{Name of country}
