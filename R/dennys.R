222e929abd72ced3cc7a0e6b714f2c97ce17c2fa
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-08-10 01:15:40 +0100

    Add partial docs

---
+++ R/dennys.R
@@ -0,0 +1,17 @@
+#' Locations of Denny's restaurants
+#'
+#' Need description 
+#'
+#' @source Need source.
+#' @format A tibble with 1643 rows and 3 variables:
+#' \describe{
+#'   \item{address}{}
+#'   \item{city}{}
+#'   \item{state}{}
+#'   \item{zip}{}
+#'   \item{longitude}{}
+#'   \item{latitude}{}
+#' }
+#' @examples
+#' dennys
+"dennys"
---
+++ R/laquinta.R
@@ -0,0 +1,17 @@
+#' Locations of La Quinta Inn motels
+#'
+#' Need description 
+#'
+#' @source Need source.
+#' @format A tibble with 909 rows and 3 variables:
+#' \describe{
+#'   \item{address}{}
+#'   \item{city}{}
+#'   \item{state}{}
+#'   \item{zip}{}
+#'   \item{longitude}{}
+#'   \item{latitude}{}
+#' }
+#' @examples
+#' laquinta
+"laquinta"
